590cda542e2ece7705a6033ea14233883ed7f67e
SWAPNIL <swapnilnigam2410@gmail.com>
2024-07-01 10:16:44 +0530

    Empty Commit

--- src/main/java/com/swapnil/AirlineService/controller/DemoController.java
+++ src/main/java/com/swapnil/AirlineService/controller/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
     @GetMapping("/")
-    public String airline(Model theModel){
+     public String airline(Model theModel){
         theModel.addAttribute("theData","Welcome to Cricket Space Airline");
         theModel.addAttribute("startJourney","from");
         theModel.addAttribute("endJourney","to");
